74261bc63b150fa9dc50b925b242cc70bcca3009
Trần Trân Lộc <tran26122003@gmail.com>
2025-07-02 16:49:31 +0700

    start project

---
+++ src/app/(admin)/admin/page.tsx
@@ -0,0 +1,3 @@
+export default function AdminHome() {
+  return <div>Đây là trang Admin</div>;
+} 
---
+++ src/app/(client)/page.tsx
@@ -0,0 +1,3 @@
+export default function ClientHome() {
+  return <div>Đây là trang Client</div>;
+} 
