dea7bcf6f54d6bd9cef20ca475fdfbfa2c1f2756
Trần Trân Lộc <tran26122003@gmail.com>
2025-07-26 22:44:12 +0700

    config Redux Toolkit

---
+++ src/lib/api.config.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+    baseURL: process.env.VITE_API_BASE_URL || 'http://localhost:8000/api/v1',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // const token = localStorage.getItem('authToken');
+    // if (token) {
+    //   config.headers.Authorization = `Bearer ${token}`;
+    // }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
